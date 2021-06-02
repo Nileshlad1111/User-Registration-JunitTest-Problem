b0b19efaa5d085f6cd04f8d9c6d40a3f9337674c
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-02 17:13:31 +0530

    ADDED:To welcome message

---
+++ src/test/java/com/brigelabz/UserRegistrationTest.java
@@ -0,0 +1,7 @@
+package com.brigelabz;
+
+public class UserRegistrationTest {
+    public static void main(String[] args) {
+        System.out.println("welcome User Registration Problem");
+    }
+}
